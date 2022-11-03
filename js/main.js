44097322b948ef0e23f484d80cd2ce0b5d8d6a2f
haidoe <haidren.amalia@gmail.com>
2022-11-02 20:37:25 -0700

    Template

---
+++ js/main.js
@@ -0,0 +1,3 @@
+// create a to-do list with checkboxes that the user can check off when a task is finished
+//Use an array of objects
+//Don't forget to include comments, and delete testing "console.log"s before submitting a zip file.
